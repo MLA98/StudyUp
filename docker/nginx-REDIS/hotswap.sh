3b61161b446b35b1970e617b924c551ae8287fda
MLA98 <leoyoudatou@outlook.com>
2019-03-13 17:47:24 -0700

    Update hotswap.sh

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-VAR= $(sed -n '/:6379/=' /etc/nginx/nginx.conf)
 if grep -q "$@:6379" /etc/nginx/nginx.conf
 then
-    echo "The IP address is already being used."
+    echo "The IP address $@:6379 is already being used."
 else
-    sed -i "$VARs/.*/    server $@:6379;/" /etc/nginx/nginx.conf
+    sed -i "$(sed -n '/:6379/=' /etc/nginx/nginx.conf)s/.*/    server $@:6379;/" /etc/nginx/nginx.conf
     /usr/sbin/nginx -s reload
 fi
